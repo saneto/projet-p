ea3c9b64c17d0da4232d06e6238401b54956a604
saneto <aymanezaircha@gmail.com>
2024-09-24 09:55:13 +0200

    Update

---
+++ context.java
@@ -0,0 +1,15 @@
+public class UserContext {
+    private static final ThreadLocal<User> currentUser = new ThreadLocal<>();
+
+    public static void setCurrentUser(User user) {
+        currentUser.set(user);
+    }
+
+    public static User getCurrentUser() {
+        return currentUser.get();
+    }
+
+    public static void clear() {
+        currentUser.remove();
+    }
+}
